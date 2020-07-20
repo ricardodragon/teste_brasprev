35df63fb1cd2c29413ddbbdc644a09e8a81cde69
ricardodragon <ricardoelfuego@gmail.com>
2020-07-20 13:47:33 -0300

    teste

--- auth/src/main/java/com/rva/auth/security/config/SecurityCredentialsConfig.java
+++ auth/src/main/java/com/rva/auth/security/config/SecurityCredentialsConfig.java
@@ -1,6 +1,5 @@
 package com.rva.auth.security.config;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import com.rva.auth.filter.JwtUsernameAndPasswordAuthenticationFilter;
@@ -21,34 +19,29 @@ import com.rva.core.properties.JwtConfiguration;
 import lombok.RequiredArgsConstructor;
 
 @EnableWebSecurity
-@RequiredArgsConstructor(onConstructor_ = {@Autowired})
-public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter{
-	
+@RequiredArgsConstructor(onConstructor_ = { @Autowired })
+public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
+
 	private final UserDetailsService userDetailsService;
 	private final JwtConfiguration jwtConfiguration;
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http
-			.csrf().disable()
-			.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
-			.and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-				.exceptionHandling().authenticationEntryPoint((req, resp, e) -> resp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-			.and()
-				.addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfiguration))
-			.authorizeRequests()
-				.antMatchers(jwtConfiguration.getLoginUrl()).permitAll()
-				.antMatchers("/loja/admin/**").hasRole("ADMIN")
-				.anyRequest().authenticated();
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.exceptionHandling()
+				.authenticationEntryPoint((req, resp, e) -> resp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+				.addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfiguration))				
+				.authorizeRequests().antMatchers(jwtConfiguration.getLoginUrl()).permitAll()
+				.antMatchers("/loja/admin/**").hasRole("ADMIN").anyRequest().authenticated();
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder builder) throws Exception {
 		// TODO Auto-generated method stub
 		builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
